18bd3b6a0eb5e646c694c6d8407359f02813f0e5
Javier Aquique <javier.aquique@gmail.com>
2019-06-09 13:48:48 -0400

    removed unecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side JS file is loading')
-
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
